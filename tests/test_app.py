dce835dd057fd924238c207b808775252d766f4f
Ahmad El Jazaerli <abe15@mail.aub.edu>
2025-11-09 22:02:39 +0200

    New files added

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+# tests/test_app.py
+from os import name
+import unittest
+from app import greet
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World!")
+if __name__ == "__main__":
+    unittest.main()
